8579cf850eff1399f15c15ec58556edbce24a059
Erastus Wainaina <164462952+wainaina-mwangi@users.noreply.github.com>
2024-09-05 13:28:41 +0300

    Add files via upload

---
+++ app/js/main.js
@@ -0,0 +1,22 @@
+const cat_btn = document.getElementById('cat_btn');
+const dog_btn = document.getElementById('dog_btn');
+const cat_result = document.getElementById('cat_result');
+const dog_result = document.getElementById('dog_result');
+
+
+dog_btn.addEventListener('click', getRandomDog);
+
+
+
+function getRandomDog() {
+	fetch('https://random.dog/woof.json')
+		.then(res => res.json())
+		.then(data => {
+			if(data.url.includes('.mp4')) {
+				getRandomDog();
+			}
+			else {
+				dog_result.innerHTML = `<img src=${data.url} alt="dog" />`;
+			}
+		});
+}
